26e2509de38afabf227a0891d7a2c82399025390
UnDoneDream <1170611254@qq.com>
2016-12-22 17:39:41 +0800

    管理中心
    管理中心首页，增加tableheaderView登录和未登录状态的View

---
+++ Chemi_客户端_3.0/Classes/Personal/View/LoginedView.m
@@ -0,0 +1,21 @@
+//
+//  LoginedView.m
+//  Chemi_客户端_3.0
+//
+//  Created by 高宇 on 16/12/22.
+//  Copyright © 2016年 ju. All rights reserved.
+//
+
+#import "LoginedView.h"
+
+@implementation LoginedView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ Chemi_客户端_3.0/Classes/Personal/View/unLoginView.h
@@ -0,0 +1,13 @@
+//
+//  unLoginView.h
+//  Chemi_客户端_3.0
+//
+//  Created by 高宇 on 16/12/22.
+//  Copyright © 2016年 ju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface unLoginView : UIView
+
+@end
